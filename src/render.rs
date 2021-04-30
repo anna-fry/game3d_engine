9b478f04bf70bf0d88f4f038b2fc66b24b4650b6
deyanamarsh <dmarsh@hmc.edu>
2021-04-29 20:07:29 -0400

    removed excess code

--- src/render.rs
+++ src/render.rs
@@ -151,23 +151,6 @@ impl Render {
         let vs_module = device.create_shader_module(&wgpu::include_spirv!("shader.vert.spv"));
         let fs_module = device.create_shader_module(&wgpu::include_spirv!("shader.frag.spv"));
        
-        // let vs_src = include_str!("2d_shader.vert");
-        // let fs_src = include_str!("2d_shader.frag");
-        // let mut compiler = shaderc::Compiler::new().unwrap();
-        // let vs_spirv = compiler.compile_into_spirv(vs_src, shaderc::ShaderKind::Vertex, "2d_shader.vert", "main", None).unwrap();
-        // let fs_spirv = compiler.compile_into_spirv(fs_src, shaderc::ShaderKind::Fragment, "2d_shader.frag", "main", None).unwrap();
-        // let vs_data = wgpu::util::make_spirv(vs_spirv.as_binary_u8());
-        // let fs_data = wgpu::util::make_spirv(fs_spirv.as_binary_u8());
-        // let vs_2d_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-        //     label: Some("Vertex Shader"),
-        //     source: vs_data,
-        //     flags: wgpu::ShaderFlags::default(),
-        // });
-        // let fs_2d_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-        //     label: Some("Fragment Shader"),
-        //     source: fs_data,
-        //     flags: wgpu::ShaderFlags::default(),
-        // });
         let vs_2d_module = device.create_shader_module(&wgpu::include_spirv!("shader_2d.vert.spv"));
         let fs_2d_module = device.create_shader_module(&wgpu::include_spirv!("shader_2d.frag.spv"));
     
